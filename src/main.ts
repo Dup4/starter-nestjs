e2d7c03560ca4c7750b5a5f68e64bd2e081a2dd8
Dup4 <lyuzhi.pan@gmail.com>
2022-08-13 11:41:22 +0800

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,46 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import path from "path";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+import { NestFactory } from "@nestjs/core";
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from "@nestjs/swagger";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { Logger } from "@nestjs/common";
+
+import { AppModule } from "./app.module";
+import packageInfo from "../package.json";
+
+const GlobalPrefix = "api";
+
+async function initSwaggerDocument(app: NestExpressApplication) {
+  const config = new DocumentBuilder()
+    .setTitle(packageInfo.name)
+    .setDescription(packageInfo.description)
+    .setVersion(packageInfo.version)
+    .addBearerAuth()
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    ignoreGlobalPrefix: false,
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup(path.join(GlobalPrefix, "docs"), app, document);
 }
 
-bootstrap();
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  initSwaggerDocument(app);
+  await app.listen(3000);
+
+  Logger.log(`${packageInfo.name} is listening on 3000`, "Bootstrap");
+}
+
+bootstrap().catch((err) => {
+  console.error(err); // eslint-disable-line no-console
+  console.error("Error bootstrapping the application, exiting..."); // eslint-disable-line no-console
+  process.exit(1);
+});
