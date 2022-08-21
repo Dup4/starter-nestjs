bf062c95e98a65a4fd3382a997a86645b109c5c8
Dup4 <lyuzhi.pan@gmail.com>
2022-08-21 19:40:45 +0800

    fix: config path

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -7,6 +7,7 @@ export STARTER_NESTJS_CONFIG_FILE=/app/config/config.yaml
 
 if [ X"${1}" = X"primary" ]; then
   if [ ! -f "/app/config/config.yaml" ]; then
+    mkdir -p /app/config
     cp /app/config-example.yaml /app/config/config.yaml
   fi
 
