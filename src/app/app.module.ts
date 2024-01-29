134e709b2dc7b4dcb6e3f5a47c291da4d8aa81e4
Duplicate4 <lyuzhi.pan@gmail.com>
2024-01-29 21:38:41 +0800

    feat: devtools (#9)
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
+import process from "node:process";
+
 import { Module } from "@nestjs/common";
+import { DevtoolsModule } from "@nestjs/devtools-integration";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -8,7 +11,15 @@ import { ConfigModule } from "@/config/config.module";
 import { HealthModule } from "@/health/health.module";
 
 @Module({
-  imports: [ConfigModule, ClusterModule, HealthModule],
+  imports: [
+    ConfigModule,
+    ClusterModule,
+    HealthModule,
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== "production",
+      port: 3013,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
