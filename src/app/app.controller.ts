6aa1c46d66f36cbb6ec1a31d6cd0380cac51d0bd
xyzyx0314 <1468790894@qq.com>
2024-02-15 23:46:28 +0700

    1

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,18 +1,14 @@
-import {
-  Controller,
-  Get,
-} from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
+
 import { AppService } from "./app.service";
 import { GetVersionResponseDto } from "./dto/get-version-response.dto";
 
-@Controller()
+@ApiTags("App")
+@Controller("app")
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-  ) { }
+  constructor(private readonly appService: AppService) { }
 
-  @ApiTags("App")
   @Get("hello")
   getHello(): string {
     return this.appService.getHello();
